55b78fa89cf3d69354f72e855f27ca3585651161
atta-design <84021777+atta-design@users.noreply.github.com>
2022-04-17 22:08:02 +0430

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,7 @@
-// import {createStore} from 'redux'
-// import Myreducer from './reducr';
-// import {composeWithDevTools} from 'redux-devtools-extension'
+
 import { configureStore } from "@reduxjs/toolkit";
 import Mreducer from './reducr'
-// const composedEnhancer=composeWithDevTools()
 
-//   export const Store=createStore(Myreducer,composedEnhancer);
 export const Store=configureStore({
   reducer:{
     shopreducer:Mreducer
